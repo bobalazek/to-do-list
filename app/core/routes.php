0c206b10011fdb993abc0612dd798ffd263606c8
Borut Balazek <bobalazek124@gmail.com>
2015-11-04 13:10:08 +0100

    Also order the fetched items by order

--- app/core/routes.php
+++ app/core/routes.php
@@ -11,7 +11,7 @@ $app->get('/', function () use ($app) {
 $app->get('/items', function () use ($app) {
     return $app->json(array(
         'success' => true,
-        'items' => $app['db']->fetchAll('SELECT * FROM items'),
+        'items' => $app['db']->fetchAll('SELECT * FROM items ORDER BY `order`'),
     ));
 });
 
